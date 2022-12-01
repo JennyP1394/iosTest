3a0b8a195680553879569afc0dd837a27e6d88c3
janki-k <>
2022-12-01 18:14:10 +0530

    remove pods

--- Pods/Target Support Files/Pods-MovieDBApp-MovieDBAppUITests/Pods-MovieDBApp-MovieDBAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MovieDBApp_MovieDBAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MovieDBApp_MovieDBAppUITests
-@end
--- Pods/Target Support Files/Pods-MovieDBApp/Pods-MovieDBApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MovieDBApp : NSObject
-@end
-@implementation PodsDummy_Pods_MovieDBApp
-@end
